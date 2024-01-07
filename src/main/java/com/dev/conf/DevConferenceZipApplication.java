36d564b8b3eab6630320a65f1a3e0d85216c17d9
seon54 <seonhyeongpark54@gmail.com>
2024-01-07 16:38:36 +0900

    feat: add a project

---
+++ src/main/java/com/dev/conf/DevConferenceZipApplication.java
@@ -0,0 +1,13 @@
+package com.dev.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevConferenceZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevConferenceZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/conf/DevConferenceZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevConferenceZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
